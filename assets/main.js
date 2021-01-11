00ceb6921159a0e803c87c38f58fd11153a8143a
kang0190 <zion-kang@hotmail.com>
2021-01-11 09:43:01 -0600

    home transparent

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,6 @@
 const navbar= document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
-    console.log(window.scrollY);
-    console.log(navbarHeight);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else{
@@ -33,3 +31,9 @@ function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+// Make Home transparent when scrollingdown
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
